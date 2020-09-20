0c90cdea6a03f864c8fee7b84899103611e5c332
Gabrielli Correa <gabriellicorrea7@gmail.com>
2020-09-20 14:13:10 -0300

    configuracao do banco de dados mySQL

--- matchlivros/src/main/java/com/matchlivros/models/Livros.java
+++ matchlivros/src/main/java/com/matchlivros/models/Livros.java
@@ -1,12 +1,34 @@
 package com.matchlivros.models;
 
-public class Livros {
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Livros implements Serializable{
+
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	private long codigo;
 	
 	private String nome;
 	private String descricao;
 	private String anolancamento;
 	private String editora;
 	
+	
+	
+	public long getCodigo() {
+		return codigo;
+	}
+	public void setCodigo(long codigo) {
+		this.codigo = codigo;
+	}
 	public String getNome() {
 		return nome;
 	}
